adc60ef00039bc4704614f13edb8ef8a785bcc2b
Matt Clements <matthewlclements@gmail.com>
2022-04-29 15:37:40 +0100

    Completed classes exercises

--- spec/counter_spec.rb
+++ spec/counter_spec.rb
@@ -1,10 +1,28 @@
 require 'counter'
 
 RSpec.describe Counter do
-  it "counter class adds num  to counter & reports" do
+  it "counter adds num  to counter & reports correctly" do
     counter = Counter.new
     counter.add(5)
     result = counter.report()
     expect(result).to eq "Counted to 5 so far."
+  end
+  
+
+  it "count is initialized at 0" do
+    counter = Counter.new
+    result = counter.report()
+    expect(result).to eq "Counted to 0 so far."
+  end
+
+  context "Given multiple nums are added" do
+    it "counter adds multiple nums corectly" do
+      counter = Counter.new
+      counter.add(5)
+      counter.add(11)
+      counter.add(2)
+      result = counter.report()
+      expect(result).to eq "Counted to 18 so far."
+    end
   end
 end
--- spec/string_builder_spec.rb
+++ spec/string_builder_spec.rb
@@ -1,5 +1,32 @@
 require 'string_builder'
 
 RSpec.describe StringBuilder do
-  it ""
+  it "return an empty string if nothing added" do
+    new_string = StringBuilder.new
+    result = new_string.output()
+    expect(result).to eq ""
+  end
+  
+
+  context "if a strings is added, it is reported" do
+    it "outputs the string added" do
+      new_string = StringBuilder.new
+      new_string.add("Shiny new string")
+      result = new_string.output()
+      expect(result).to eq "Shiny new string"
+    end
+  end
+  
+  context "if mutltiple strings are added" do
+    it "it will report multiple strings" do
+      new_string = StringBuilder.new
+      new_string.add("look ")
+      new_string.add("more ")
+      new_string.add("strings ")
+      new_string.add("get ")
+      new_string.add("added")
+      result = new_string.output()
+      expect(result).to eq "look more strings get added"
+    end
+  end
 end
