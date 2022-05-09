f4c19c8a2123c25ea56ccec5078f4d62c093d6d0
Matt Clements <matthewlclements@gmail.com>
2022-05-09 15:07:24 +0100

    Finished Phase2-7

--- lib/letter_counter.rb
+++ lib/letter_counter.rb
@@ -1,19 +1,25 @@
 class LetterCounter
+  attr_accessor :is_letter?
   def initialize(text)
     @text = text
   end
 
   def calculate_most_common()
-    counter = Hash.new(1)
+    counter = Hash.new(0)
     most_common = nil
-    most_common_count = 1
+    most_common_count = 0
+  
     @text.chars.each do |char|
+
       next unless is_letter?(char)
       counter[char] = (counter[char] || 1) + 1
+    
       if counter[char] > most_common_count
         most_common = char
-        most_common_count += counter[char]
+        most_common_count += 1
       end
+    
+    
     end
     return [most_common_count, most_common]
   end
@@ -25,7 +31,7 @@ class LetterCounter
   end
 end
 
-counter = LetterCounter.new("Digital Punk")
+counter = LetterCounter.new("IIII bb a 8")
 p counter.calculate_most_common
 
 # Intended output:
